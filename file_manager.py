102608e6fa4ef6d1b9dfb5c5e64b22be706d06bd
Juan peng <juanpeng94@gmail.com>
2023-11-22 10:52:25 +0800

    added config file to retrieve where each board data's query_template and outputpath should be stored

--- file_manager.py
+++ file_manager.py
@@ -3,23 +3,20 @@ import os
 
 class FileManager:
     """Manages file operations for data storage."""
-    # Define the default folder path as a class attribute
-    DEFAULT_FOLDER_PATH = r'C:\Users\Juan Peng\monscript\storage'
-    
-    @classmethod
-    def ensure_directory_exists(cls):
-        if not os.path.exists(cls.DEFAULT_FOLDER_PATH):
-            os.makedirs(cls.DEFAULT_FOLDER_PATH)
+    @staticmethod
+    def ensure_directory_exists(directory_path):
+        if not os.path.exists(directory_path):
+            os.makedirs(directory_path)
 
-    @classmethod
-    def save_to_json(cls, data, filename):
-        cls.ensure_directory_exists()  # Make sure the directory exists
-        full_path = os.path.join(cls.DEFAULT_FOLDER_PATH, filename)
+    staticmethod
+    def save_to_json(data, filename, directory_path):
+        FileManager.ensure_directory_exists(directory_path)  # Make sure the directory exists
+        full_path = os.path.join(directory_path, filename)
         with open(full_path, 'w') as f:
             json.dump(data, f, indent=4)
 
-    @classmethod
-    def save_to_csv(cls, dataframe, filename):
-        cls.ensure_directory_exists()  # Make sure the directory exists
-        full_path = os.path.join(cls.DEFAULT_FOLDER_PATH, filename)
+    @staticmethod
+    def save_to_csv(dataframe, filename, directory_path):
+        FileManager.ensure_directory_exists(directory_path)
+        full_path = os.path.join(directory_path, filename)
         dataframe.to_csv(full_path, index=False)
--- main.py
+++ main.py
@@ -12,34 +12,79 @@ from api_client import MondayApiClient
 from data_handler import DataHandler
 from file_manager import FileManager
 
-# Convert the response content into a dictionary
+# Load configuration from JSON
+def load_config(config_path):
+    with open(config_path, 'r') as file:
+        return json.load(file)
+
 def main():
-    # setup api varaibles
+    # Load environment variables
     load_dotenv("apikey.env")
     api_key = os.getenv('API_KEY')
     api_url = os.getenv('API_URL')
-    headers = {"Authorization" : api_key}
+    headers = {"Authorization": api_key}
 
-    # initialise mondayAPIClient class and load query template
-    client = MondayApiClient(api_url=api_url, headers=headers)
-    current_dir = os.path.dirname(__file__)
-    template_path = os.path.join(current_dir, 'templates', 'stats.graphql')
-    client.add_query_template('stats', template_path)
+    # Initialize MondayApiClient
+    monday_client = MondayApiClient(api_url, headers)
 
-    # query from specific board via board id
-    board_id = 5100178786 # this should be a call so we know which boards to get
-    query_items = client.load_query('stats', board_id=board_id)
+    # Load config.json
+    config = load_config('config.json')
 
-    # get board data as a json file
-    boards_data = client.fetch_data(query_items)
-    FileManager.save_to_json(boards_data, 'stats.json')
+ # Iterate over each board configuration
+    for board_config in config['boards']:
+        board_id = board_config['board_id']
+        query_template_identifier = board_config['query_template']
+        output_path = board_config['output_path']
 
-    # get board data as a tabular csv
-    items_data = client.fetch_data(query_items)
-    items = items_data["data"]["boards"][0]["items"]
-    processed_items = DataHandler.process_items_to_dict(items)
-    df = DataHandler.convert_to_dataframe(processed_items)
-    FileManager.save_to_csv(df, 'board_items.csv')
+        # Construct the path to the query template file
+        template_file_path = f"templates/{query_template_identifier}.graphql"
+
+        # Add and load query template
+        monday_client.add_query_template(query_template_identifier, template_file_path)
+        query = monday_client.load_query(query_template_identifier, board_id=board_id)
+
+        # Fetch data
+        board_data = monday_client.fetch_data(query)
+
+        # Process data
+        items = board_data["data"]["boards"][0]["items"]
+        processed_items = DataHandler.process_items_to_dict(items)
+        data_frame = DataHandler.convert_to_dataframe(processed_items)
+
+        # Construct filename for CSV
+        csv_filename = f"{board_id}.csv"
+        json_filename = f"{board_id}.json"
+
+        # Save to CSV in the specified output path
+        FileManager.save_to_csv(data_frame, csv_filename, output_path)
+        FileManager.save_to_json(board_data, json_filename, output_path)
+        # # Check if data was retrieved
+        # if not board_data:
+        #     print(f"No data retrieved for board ID {board_config['board_id']}")
+        #     continue  # Skip to the next board if no data
+
+        # # Process data
+        # items = board_data.get("data", {}).get("items", [])
+        # if not items:
+        #     print(f"No items to process for board ID {board_config['board_id']}")
+        #     continue  # Skip to the next board if no items
+
+        # processed_items = DataHandler.process_items_to_dict(items)
+        # data_frame = DataHandler.convert_to_dataframe(processed_items)
+
+        # if data_frame.empty:
+        #     print(f"Data frame is empty for board ID {board_config['board_id']}")
+        #     continue  # Skip to the next board if DataFrame is empty
+
+        # # Construct filename for CSV
+        # csv_filename = f"{board_config['board_id']}.csv"
+
+        # # Save to CSV
+        # try:
+        #     FileManager.save_to_csv(data_frame, csv_filename, board_config['output_path'])
+        #     print(f"Data saved to {os.path.join(board_config['output_path'], csv_filename)}")
+        # except Exception as e:
+        #     print(f"Failed to save data for board ID {board_config['board_id']}: {e}")
 
 if __name__ == '__main__':
     main()
